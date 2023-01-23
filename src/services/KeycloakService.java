0e31f167149548c9b07b322f193968a901883a70
nikisV <nikisvedenyapin@gmail.com>
2023-01-23 09:27:43 +0300

    fix

--- src/services/KeycloakService.java
+++ src/services/KeycloakService.java
@@ -5,19 +5,28 @@ import org.jboss.resteasy.client.jaxrs.internal.ResteasyClientBuilderImpl;
 import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
 import org.keycloak.representations.idm.UserRepresentation;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class KeycloakService {
 
+  @Value("${keycloak.auth-server-url}")
   private String SERVER_URL;
 
+  @Value("${keycloak.realm}")
   private String REALM;
 
+  @Value("${kc.user.username}")
   private String USERNAME;
 
+  @Value("${kc.user.password}")
   private String PASSWORD;
 
+  @Value("${keycloak.resource}")
   private String CLIENT_ID;
 
+  @Value("${kc.admin.realm}")
   private String MASTER_REALM;
 
   private Keycloak getInstance() {
