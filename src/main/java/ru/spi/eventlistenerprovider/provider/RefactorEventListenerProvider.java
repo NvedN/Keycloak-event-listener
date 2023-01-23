db9deb59c82aa8b30de8998cc1fc93acd3380354
nikisV <nikisvedenyapin@gmail.com>
2023-01-23 09:05:20 +0300

    feat: send async reqest, handling of additional events, including admin events

--- src/main/java/ru/spi/eventlistenerprovider/provider/RefactorEventListenerProvider.java
+++ src/main/java/ru/spi/eventlistenerprovider/provider/RefactorEventListenerProvider.java
@@ -2,11 +2,14 @@ package ru.spi.eventlistenerprovider.provider;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.keycloak.events.Event;
 import org.keycloak.events.EventListenerProvider;
 import org.keycloak.events.EventType;
 import org.keycloak.events.admin.AdminEvent;
+import org.keycloak.events.admin.OperationType;
+import org.keycloak.events.admin.ResourceType;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.RealmProvider;
@@ -27,35 +30,99 @@ public class RefactorEventListenerProvider implements EventListenerProvider {
   // documentation keycloak SPI events
   @Override
   public void onEvent(Event event) {
-    if (EventType.REGISTER.equals(event.getType())) {
-
-      log.info("## NEW %s EVENT" + event.getType());
-      log.info("-----------------------------------------------------------");
-
-      RealmModel realm = this.model.getRealm(event.getRealmId());
-      UserModel newRegisteredUser = this.session.users().getUserById(realm, event.getUserId());
-
-      UserDTO newUser =
-          new UserDTO(
-              newRegisteredUser.getUsername(),
-              newRegisteredUser.getEmail(),
-              newRegisteredUser.getCreatedTimestamp(),
-              newRegisteredUser.getFirstName(),
-              newRegisteredUser.getLastName());
-      try {
-        CustomRequest.sendRequest(newUser, newRegisteredUser, session);
-
-      } catch (RuntimeException e) {
-        log.error("Failed to send http request" + e);
-      } catch (IOException | InterruptedException e) {
-        throw new RuntimeException(e);
-      }
-      log.info("-----------------------------------------------------------");
+    if (EventType.REGISTER.equals(event.getType())
+        || EventType.UPDATE_PROFILE.equals(event.getType())
+        || EventType.DELETE_ACCOUNT.equals(event.getType())) {
+      prepareDataAndSendRequest(event.getRealmId(), event.getUserId(), null, event);
     }
   }
 
   @Override
-  public void onEvent(AdminEvent event, boolean includeRepresentation) {}
+  public void onEvent(AdminEvent event, boolean includeRepresentation) {
+
+    if (ResourceType.USER.equals(event.getResourceType())
+        && (OperationType.DELETE.equals(event.getOperationType())
+        || OperationType.CREATE.equals(event.getOperationType()))
+        || OperationType.UPDATE.equals(event.getOperationType())) {
+      String resourcePath = event.getResourcePath();
+      if (resourcePath.startsWith("users/")) {
+        prepareDataAndSendRequest(
+            event.getRealmId(), resourcePath.substring("users/".length()), event, null);
+      }
+    }
+  }
+
+
+  /**
+   * Method to convert EventType or OperationType object to constant string with event type
+   *
+   * @param adminEvent {@code AdminEvent} AdminEvent object
+   * @param event {@code Event} Event object
+   * @return string type of event
+   * @author NVN
+   * @since 2023.01.09
+   */
+  public String defineEventType(AdminEvent adminEvent, Event event) {
+    String returnValue = null;
+    if (event != null) {
+      if (EventType.REGISTER.equals(event.getType())) {
+        returnValue = CustomRequest.REGISTER;
+      } else if (EventType.UPDATE_PROFILE.equals(event.getType())) {
+        returnValue = CustomRequest.UPDATE;
+      } else if (EventType.DELETE_ACCOUNT.equals(event.getType())) {
+        returnValue = CustomRequest.DELETE;
+      }
+    } else {
+      if (OperationType.CREATE.equals(adminEvent.getOperationType())) {
+        returnValue = CustomRequest.REGISTER;
+      } else if (OperationType.UPDATE.equals(adminEvent.getOperationType())) {
+        returnValue = CustomRequest.UPDATE;
+      } else if ((OperationType.DELETE.equals(adminEvent.getOperationType()))) {
+        returnValue = CustomRequest.DELETE;
+      }
+    }
+    return returnValue;
+  }
+
+  /**
+   * Method to prepare data and send request
+   *
+   * @param realmId {@code String}
+   * @param userId {@code String}
+   * @param adminEvent {@code AdminEvent}
+   * @param event {@code Event}
+   * @author NVN
+   * @since 2023.01.09
+   */
+  private void prepareDataAndSendRequest(
+      String realmId, String userId, AdminEvent adminEvent, Event event){
+    RealmModel realm = session.realms().getRealm(realmId);
+    UserModel userModel = session.users().getUserById(realm, userId);
+    String eventType;
+    if (adminEvent != null) {
+      eventType = defineEventType(adminEvent, null);
+    } else {
+      eventType = defineEventType(null, event);
+    }
+    UserDTO user;
+    if (CustomRequest.DELETE.equals(eventType)) {
+      user = new UserDTO(UUID.fromString(userId));
+    } else {
+      user =
+          new UserDTO(
+              UUID.fromString(userId),
+              userModel.getUsername(),
+              userModel.getEmail(),
+              userModel.getCreatedTimestamp(),
+              userModel.getFirstName(),
+              userModel.getLastName());
+    }
+    try {
+      CustomRequest.sendRequest(user, session, eventType);
+    } catch (Exception e) {
+      log.error("Failed to send http request" + e);
+    }
+  }
 
   private String toString(Event event) {
     StringBuilder sb = new StringBuilder();
